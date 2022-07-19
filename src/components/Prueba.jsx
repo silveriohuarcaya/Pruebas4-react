fdf94ce5880d07834bd722ed6aa96f7726e9dc28
silveriohuarcaya <20161844@une.edu.pe>
2022-07-18 23:04:54 -0500

    Mi configuracion ideal (#1)

---
+++ src/components/Prueba.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Prueba = () => {
+  const a = 4
+  const as = 12
+  return (
+    <div className="div">
+      <h3>
+        MAria - {a} - {as}
+      </h3>
+    </div>
+  )
+}
+export default Prueba
