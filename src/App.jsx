82b37b33827db1982a94fb6efa922c88774aee9c
silveriohuarcaya <corwilgi@gmail.com>
2022-09-11 00:47:56 -0500

    fix (#5)
    Co-authored-by: Silverio Huarcaya <corwilgi@Silverio.localdomain>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+import Brother from './components/Brother'
+import Sister from './components/Sister'
+
 const App = () => {
-  const a = 5
+  const [message, setMessage] = useState('Papa')
+
+  const handleMessage = (msg) => {
+    setMessage(msg)
+  }
+
   return (
     <div className="App">
-      <h1>Hola - {a} </h1>
+      <h1>{message}</h1>
+      <Brother greeting={message} onHandleMessage={handleMessage} />
+      <Sister greeting={message} onHandleMessage={handleMessage} />
     </div>
   )
 }
--- src/AppUseStateInputProps.jsx
+++ src/AppUseStateInputProps.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 
-import Brother from './components/Brother'
-import Sister from './components/Sister'
-
 const App = () => {
-  const [message, setMessage] = useState('Papa')
-
-  const handleMessage = (msg) => {
-    setMessage(msg)
-  }
-
+  const a = 5
   return (
     <div className="App">
-      <h1>{message}</h1>
-      <Brother greeting={message} onHandleMessage={handleMessage} />
-      <Sister greeting={message} onHandleMessage={handleMessage} />
+      <h1>Hola - {a} </h1>
     </div>
   )
 }
